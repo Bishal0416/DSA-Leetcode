17b89fb0e89cae1247fcd54d117e243c091a6aa8
BigB <128973794+Bishal0416@users.noreply.github.com>
2024-04-18 13:23:35 +0530

    Time: 51 ms (16.28%) | Memory: 58.6 MB (42.25%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -5,23 +5,39 @@ public:
         int i =0, j=0;
         int len =0;
         int skip =0;
+        int last_zero_idx=-1;
         int k=1;
-
+//----------------------------Type-I--------------------------//
         while(j<n){
-            if(nums[j] != 1){
-                skip++;
+            if(nums[j] == 0){
+                i = last_zero_idx + 1;
+                last_zero_idx = j;
             }
-            while(skip > k){
-                if(nums[i] != 1){
-                    skip--;
-                }
-                i++;
-            }
-
-            len = max(len, j-i+1);
+            len = max(len, j-i);
             j++;
         }
 
-        return len-1;
+
+
+
+        //-----------------type-II-----------------------------//
+        // while(j<n){
+        //     if(nums[j] != 1){
+        //         skip++;
+        //     }
+        //     while(skip > k){
+        //         if(nums[i] != 1){
+        //             skip--;
+        //         }
+        //         i++;
+        //     }
+
+        //     len = max(len, j-i+1);
+        //     j++;
+        // }
+
+        // return len-1; // for type-II;
+
+        return len; //for type-I
     }
 };
