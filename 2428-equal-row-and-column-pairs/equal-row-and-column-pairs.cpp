1df83e5ea7fc8713593c094885e5f3cc14e3e2a4
BigB <128973794+Bishal0416@users.noreply.github.com>
2024-04-20 13:07:48 +0530

    Time: 59 ms (87.32%) | Memory: 35.3 MB (48.75%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
+
+//----------------------------------O(n^3)----------------------------------------//
+        // int n = grid.size();
+        // int count = 0;
+
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<n; j++){
+        //         bool match = true;
+        //         for(int k=0; k<n; k++){
+        //             if(grid[i][k] != grid[k][j]){
+        //                 match = false;
+        //                 break;
+        //             }
+        //         }
+        //         if(match) count++;
+        //     }
+        // }
+        // return count;
+
+//---------------------------------O(n^2) + O(n^2)---------------------------------------//
         int n = grid.size();
         int count = 0;
+        map<vector<int>, int> prev;
 
         for(int i=0; i<n; i++){
+            prev[grid[i]]++;
+        }
+
+        for(int i =0; i<n; i++){
+            vector<int> temp;
             for(int j=0; j<n; j++){
-                bool match = true;
-                for(int k=0; k<n; k++){
-                    if(grid[i][k] != grid[k][j]){
-                        match = false;
-                        break;
-                    }
-                }
-                if(match) count++;
+                temp.push_back(grid[j][i]);
             }
+            count += prev[temp];
         }
         return count;
     }
